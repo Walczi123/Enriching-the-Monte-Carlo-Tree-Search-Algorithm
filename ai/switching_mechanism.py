5c4059bd8e12be5649c576cace551d1cd223bb5a
Walczi123 <patryk.wal@onet.pl>
2022-10-01 13:03:54 +0200

    config change and switching fix

--- ai/switching_mechanism.py
+++ ai/switching_mechanism.py
@@ -1,9 +1,10 @@
 from math import log, sqrt
+import random
 
 class SwitchingMechanism():
     def __init__(self, strategies):
         self.strategies = strategies
-        self.b = 4
+        self.b = 5 # wczesniej 4
         self.q_table = [0] * len(strategies)
         self.t_table = [0] * len(strategies)
 
@@ -13,13 +14,20 @@ class SwitchingMechanism():
         return 1
 
     def choose_strategy(self, n):
+        if self.t_table.count(0) > 0:
+            index_list = [index for index, value in enumerate(self.t_table) if value == 0]
+            return self.strategies[random.choice(index_list)]
         result = []
         for i in range(len(self.strategies)):
-            result.append(self.q_table[i] + self.b * sqrt(log(n+1)/self.__t_table__(i)))
+            result.append((self.q_table[i]/self.__t_table__(i)) + self.b * sqrt(log(n)/self.__t_table__(i)))
         index = result.index(max(result))
+        print("result", result)
+        print("max(result)", max(result))
+        print("result.index(max(result))", result.index(max(result)))
         return self.strategies[index]
 
     def update_strategy_result(self, strategy, result):
         index = self.strategies.index(strategy)
         self.t_table[index] += 1
         self.q_table[index] += result
+
--- play_othello.py
+++ play_othello.py
@@ -1,20 +1,24 @@
 
 import random
 import time
+from ai.switching_mechanism import SwitchingMechanism
 from games.othello.evaluate import othello_evaluate
 from games.othello.othello import Othello
 from games.othello.othello_player import Greedy_Othello_Player, MapBaseHeu_Othello_Player
 from games.player import AlphaBeta_Player, MCTSRAVE_Player, MCTS_Player, Man_Player, Random_Player, MCTSStrategy_Player, MCTSSwitchingStrategy_Player, Strategy_Player
-from strategies.othello_strategies import mapbaseothello_strategy
+from strategies.othello_strategies import evaluateothello_strategy, greedyothello_strategy, mapbaseothello_strategy
 from strategies.strategies import mobility_strategy, mobility_strategy_vs, random_strategy
 
 if __name__ == "__main__":
     random.seed(22025005)
     start_time = time.time()
-    p1 = AlphaBeta_Player(othello_evaluate, 4)
+    p1 = MCTSSwitchingStrategy_Player([random_strategy, mobility_strategy_vs,mobility_strategy,mapbaseothello_strategy,greedyothello_strategy,evaluateothello_strategy], 10)
     # p2 = Strategy_Player(mapbaseothello_strategy)
-    p2 = MCTS_Player(10)
+    p2 = Man_Player()
 
-    game = Othello(use_ui=False, player1=p1, player2=p2)
+    game = Othello(use_ui=True, player1=p1, player2=p2)
     print(game.play())
     print("--- %s seconds ---" % (time.time() - start_time))
+
+#    sm = SwitchingMechanism([random_strategy, mobility_strategy_vs,mobility_strategy,mapbaseothello_strategy,greedyothello_strategy,evaluateothello_strategy])
+#    sm.choose_strategy(0)
